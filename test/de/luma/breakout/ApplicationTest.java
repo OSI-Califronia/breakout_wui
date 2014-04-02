54ea46dabcea668f6f6a2d65571a8ce2a618d947
MBausch <MBausch@LAPTOP-MBAUSCH.dckasasi.local>
2014-04-02 14:03:44 +0200

    refactor test packages

--- test/de/luma/breakout/ApplicationTest.java
+++ test/de/luma/breakout/ApplicationTest.java
@@ -1,3 +1,4 @@
+package de.luma.breakout;
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.contentAsString;
 import static play.test.Helpers.contentType;
--- test/de/luma/breakout/IntegrationTest.java
+++ test/de/luma/breakout/IntegrationTest.java
@@ -1,3 +1,4 @@
+package de.luma.breakout;
 import org.junit.*;
 
 import play.test.*;
